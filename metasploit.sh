839f56240765ad840351662e42d8b41e085457d7
sqlasskicker <63858650+sqlasskicker@users.noreply.github.com>
2020-09-08 18:34:46 +0530

    Add

---
+++ metasploit.sh
@@ -0,0 +1,44 @@
+#---colours---
+red='\033[1;91m'
+green='\033[1;92m'
+yellow='\033[1;93m'
+blue='\033[1;94m'
+purple='\033[1;95m'
+cyan='\033[1;96m'
+white='\033[1;97m'
+
+clear
+
+echo ""
+echo -e "\033[1;93m   ╔╦╗┌─┐┌┬┐┌─┐   ╦┌┐┌┌─┐┌┬┐┌─┐┬  ┬  ┌─┐┬─┐"
+echo -e "\033[1;93m   ║║║├┤  │ ├─┤ - ║│││└─┐ │ ├─┤│  │  ├┤ ├┬┘"
+echo -e "\033[1;93m   ╩ ╩└─┘ ┴ ┴ ┴   ╩┘└┘└─┘ ┴ ┴ ┴┴─┘┴─┘└─┘┴└─"
+echo -e             "<<================××××===================>>"
+echo ""
+
+echo " "
+echo -e "\033[1;91m  • Author Information •"
+echo -e " \033[1;91m  ××××××××××××××××××××"
+echo -e " \033[1;91m> Name    : SQL ASSKICKER"
+echo -e "\033[1;91m > Github  : https://github.com/sqlasskicker"
+echo -e "\033[1;91m > YouTube : https://www.youtube.com/channel/UCCon0vRPbg42z7hW5"
+echo -e "\033[1;91m > STAY TUNED .."
+echo -e "\033[1;91m   ××××××××××××××××××××××"
+echo " "
+
+echo -e "\033[1;92m [•] Only for Android 7 and above"
+echo ""
+echo -e "\033[1;95m  Wait installing ...."
+echo ""
+
+pkg update
+pkg install repo
+pkg install unstable-repo
+pkg install metasploit
+clear
+
+echo ""
+echo -e "\033[1;95m    [•]  Installation Finished !"
+echo ""
+echo -e "\033[1;92m    [•] Type msfconsole to use Metasploit"
+echo""
